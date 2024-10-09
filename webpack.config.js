d758c4fa1578d1e95e383b8df648a331281c5f4d
pirulug <pirulug@gmail.com>
2024-10-09 10:29:16 -0500

    update 0.0.6

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { BannerPlugin } = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: { prims: "./src/js/app.js" },
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  // mode: "development",
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false, // Elimina otros comentarios
+            preamble: `/*! 
+    * PiruBbEditor (https://github.com/pirulug)
+    * Copyright 2024 Pirulug (https://github.com/pirulug)
+    * Licensed under MIT
+    */`,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+  plugins: [
+    // DELETE
+    new CleanWebpackPlugin(),
+
+    // Banner
+    new BannerPlugin({
+      banner: `/*!
+  * PiruBbEditor (https://github.com/pirulug)
+  * Copyright 2024 Pirulug (https://github.com/pirulug)
+  * Licensed under MIT
+  */`,
+      raw: true,
+      entryOnly: false,
+    }),
+
+    // CSS
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css", // Nombre del archivo CSS
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, //asd
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: "svg-inline-loader",
+      },
+    ],
+  },
+};
